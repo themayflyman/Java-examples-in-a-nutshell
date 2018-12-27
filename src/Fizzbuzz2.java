1050da51913ac3a4c6a2d2a5d732d6251c162878
JoJo <xuziwei100@gmail.com>
2018-12-27 14:55:49 +0800

    Example 1-6： FizzBuzz2.java

---
+++ src/Fizzbuzz2.java
@@ -0,0 +1,25 @@
+/**
+ * This class is much like the FizzBuzz class, but uses a switch statement
+ * instead of repeated if/else statements.
+ **/
+public class Fizzbuzz2 {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++) {
+            switch(i % 35) {
+                case 0:
+                    System.out.print("fizzbuss ");
+                    break;
+                case 5: case 10: case 15:
+                case 20: case 25: case 30:
+                    System.out.print("fizz ");
+                    break;
+                case 7: case 14: case 21: case 28:
+                    System.out.print("buzz ");
+                default:
+                    System.out.print(i + " ");
+                    break;
+            }
+        }
+        System.out.println();
+    }
+}
