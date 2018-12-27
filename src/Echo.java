71ae4ac9891a8b21d2476f436554b637b7322fe6
JoJo <xuziwei100@gmail.com>
2018-12-27 14:31:27 +0800

    4

---
+++ src/Echo.java
@@ -0,0 +1,13 @@
+/**
+ * This program prints out all its command-line arguments.
+ **/
+public class Echo {
+    public static void main(String[] args) {
+       int i = 0;                            // Initialize the loop variable
+       while(i < args.length) {              // Loop until the end of array
+           System.out.print(args[i] + " ");  // Print each argument out
+           i++;                              // Increment the loop variable
+       }
+       System.out.println();                 // Terminate the line
+    }
+}
---
+++ src/FizzBuzz.java
@@ -0,0 +1,21 @@
+/**
+ * This program plays the game "FizzBuzz". It counts to 100, replacing each
+ * multiple of 5 with the word "fizz", each multiple of 7 with the word "buzz",
+ * and each multiple of both with the word "fizzbuzz". It uses the modulo
+ * operator (%) to determine if a number is divisible by another.
+ **/
+public class FizzBuzz {
+    public static void main(String[] args){
+        for(int i = 1; i <= 100; i++) {
+            if (((i % 5) == 0) && ((i % 7) == 0))
+                System.out.println("fizzbuss");
+            else if ((i % 5) == 0)
+                System.out.println("fizz");
+            else if ((i % 7) == 0)
+                System.out.println("buzz");
+            else System.out.println(i);
+            System.out.println(" ");
+        }
+        System.out.println();
+    }
+}
