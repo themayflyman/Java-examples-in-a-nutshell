66906d606b22f8a38a1ef97fe6ec72bc9070da20
JoJo <xuziwei100@gmail.com>
2018-12-27 21:12:30 +0800

    Example 1-12： FactQuoter.java

---
+++ src/FactQuoter.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+/**
+ * This program displays factorials as the user enters values interactively
+ */
+public class FactQuoter {
+    public static void main(String[] args) throws IOException {
+        // This is how we set things up to real lines of text
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        // Loop forever
+        for(;;) {
+            // Display a prompt to the user
+            System.out.print("<FactQuoter> ");
+            // Read a line from the user
+            String line = in.readLine();
+            // If we reach the end-of-file,
+            // or if the user types "quit", then quit
+            if ((line == null) || line.equals("quit")) break;
+            // Try to parse the line, and compute and print the factorial
+            try {
+                int x = Integer.parseInt(line);
+                System.out.println(x + "! = " + Factorial4.factorial(x));
+            }
+            // If anything goes wrong, display a generic error message
+            catch (Exception e) {System.out.println("Invalid Input");}
+        }
+    }
+}
