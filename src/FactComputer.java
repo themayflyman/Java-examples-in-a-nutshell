244f431b6eb496762bb4ddf5366fd706f1d4f247
JoJo <xuziwei100@gmail.com>
2018-12-27 20:53:25 +0800

    Example 1-11： FactComputer.java

---
+++ src/FactComputer.java
@@ -0,0 +1,31 @@
+/**
+ * This program computes and displays the factorial of a number specified
+ * on the command line. It handles possible user input errors with try/catch.
+ */
+public class FactComputer {
+    public static void main(String[] args) {
+        // Try to compute a factorial
+        // If something goes wrong, handle it in the catch clause below
+        try {
+            int x = Integer.parseInt(args[0]);
+            System.out.println(x + "! = " + Factorial4.factorial(x));
+        }
+        // The user forgot to specify an argument
+        // Throw if args[0] if undefined
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("You must specify an argument");
+            System.out.println("Usage: java FactComputer <number>");
+        }
+        // The argument is not an number
+        // Thrown by Integer.parseInt()
+        catch (NumberFormatException e) {
+            System.out.println("The argument you specify must be an integer");
+        }
+        // The argument is < 0.
+        // Thrown by Factorial4.factorial()
+        catch (IllegalArgumentException e) {
+            // Display the message sent by the factorial() method
+            System.out.println("Bad argument: " + e.getMessage());
+        }
+    }
+}
