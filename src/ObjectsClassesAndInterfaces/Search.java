99ac0aa827e936aa390cad9fd1460bc8c0cab0dc
JoJo <xuziwei100@gmail.com>
2018-12-31 15:47:28 +0800

    Exercise 2-3: Search.java

--- src/ObjectsClassesAndInterfaces/Search.java
+++ src/ObjectsClassesAndInterfaces/Search.java
@@ -2,6 +2,14 @@ package ObjectsClassesAndInterfaces;
 
 public class Search {
     public Object search(Object[] a, Object b, Sorter.Comparer c) {
-        return -1;
+
+    }
+
+    public static class Test {
+        static class SortableCircle extends Circle implements Sorter.Comparable {
+            public SortableCircle(int radius, Circle.Dot center) { super(radius, center); }
+            public int compareTo(Object other) { return radius - ((Circle)other).radius;}
+        }
+
     }
 }
