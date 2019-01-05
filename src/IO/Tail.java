8b9016f8129815093f24642664919d13d866b36e
JoJo <xuziwei100@gmail.com>
2019-01-05 19:50:00 +0800

    Exercise 3-2: Tail.java

--- src/IO/Tail.java
+++ src/IO/Tail.java
@@ -20,22 +20,40 @@ public class Tail {
 
     public static void tail(String filename) {
         File f;
+        // A buffered reader to count the total lines of the file
         BufferedReader br = null;
+        // A buffered reader to read or print the file
+        BufferedReader br2 = null;
 
         try {
             f = new File(filename);
             br = new BufferedReader(new FileReader(f));
-
-            while(line <= 10) {
-                System.out.println(line + " " + br.readLine());
-                line++;
+            br2 = new BufferedReader(new FileReader(f));
+            String str = null;
+            long len = 0;
+            while((str = br.readLine()) != null)
+                len++;
+            if(len < 10) {
+                for(int i=0; i<len; i++)
+                    System.out.println(i + " " + br2.readLine());
+            }
+            else {
+                for(long line=1;line<len; line++)
+                    if(line > len - 10)
+                        System.out.println(line + " " + br2.readLine());
+                    else
+                        br2.readLine();
             }
         }
         catch(IOException e) { System.err.println(e.getMessage()); }
         finally {
             if(br != null){
                 try { br.close(); }
-                catch(IOException e1) {}
+                catch(IOException e1) { System.err.println(e1.getMessage()); }
+            }
+            if(br2 != null){
+                try { br2.close(); }
+                catch(IOException e2) { System.err.println(e2.getMessage()); }
             }
         }
     }
