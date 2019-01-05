fd8555a46cb4626d4e88118bcf77bf5aa481a0db
JoJo <xuziwei100@gmail.com>
2019-01-05 19:25:12 +0800

    Exercise 3-1: Head.java

--- src/IO/Tail.java
+++ src/IO/Tail.java
@@ -2,35 +2,30 @@ package IO;
 import java.io.*;
 
 /**
- * A program that prints out the first 10 lines of each file specified on the command line
+ * A program that prints out the last 10 lines of each file specified on the command line
  */
-public class Head {
-    /**
-     * This is main() method of the standalone program. After checking
-     * its arguments, it invokes the Head.head() to prints out the first 10 lines of the file
-     */
+public class Tail {
     public static void main(String[] args) {
-        if(args.length != 1) {         // Check command-line arguments
+        if (args.length != 1) {         // Check command-line arguments
             System.err.println("Usage: java Head <filename>");
             System.exit(0);
         }
-        // Call the head() method
-        try { head(args[0]); }
-        catch(IllegalArgumentException e) {
+        // Call the tail() method
+        try {
+            tail(args[0]);
+        } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
         }
     }
 
-    public static void head(String filename) {
+    public static void tail(String filename) {
         File f;
         BufferedReader br = null;
 
         try {
             f = new File(filename);
             br = new BufferedReader(new FileReader(f));
-            int line = 1;
 
-            // breaks when null is read or the first 10 lines have been printed
             while(line <= 10) {
                 System.out.println(line + " " + br.readLine());
                 line++;
